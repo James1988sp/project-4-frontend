169b2b918123f32a89a97e8bca0792fef5b91fd1
James1988sp <gianmariacarrodano@libero.it>
2017-04-24 13:13:56 +0100

    Styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 angular
-  .module('droneApp', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate'])
+  .module('droneApp', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ngAnimate'])
   .constant('API_URL', 'http://localhost:3000/api');
